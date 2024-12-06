573d4ac337deb1b09e75aa5c2e2762892e7035c9
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2024-12-06 16:26:04 +0530

    installing pkgs

--- client/src/pages/components/Navbar.tsx
+++ client/src/pages/components/Navbar.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from '/images/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex "></nav>
   )
 }
 
